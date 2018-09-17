620bb036568b3f3e372dfacd242a287ca5305ab9
clee088 <42843045+clee088@users.noreply.github.com>
2018-09-17 16:09:52 -0400

    Updated from Brython Server: 9/17/2018 4:09:52 PM

--- cryptography.py
+++ cryptography.py
@@ -1,6 +1,6 @@
 """
 cryptography.py
-Author: <your name here>
+Author: Christopher Lee
 Credit: <list sources used, if any>
 
 Assignment:
@@ -10,3 +10,24 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+
+def prompt():
+    ask = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+    for i in ask:
+        if i == 'e' or i == 'd' or i == 'q':
+            msg = input('Message: ')
+            key = input('Key: ')
+            if len(msg) > len(key):
+                for l in msg:
+                    (associations.find(l) + associations.find(key))
+            else:
+                for l in msg:
+                    ekey = (associations.find(l) + associations.find(key))
+                for e in ekey:
+                    k = associations[e]
+                print(k)
+        else:
+            print('Did not understand command, try again.')
+            prompt()
+prompt()
