66dec100cd63afa66d5bad9593ac1d8a66513806
clee088 <42843045+clee088@users.noreply.github.com>
2018-09-18 19:48:12 -0400

    Updated from Brython Server: 9/18/2018 7:48:11 PM

--- cryptography.py
+++ cryptography.py
@@ -20,17 +20,38 @@ def prompt():
             msg = input('Message: ')
             key = input('Key: ')
             p = ''
-            for k in key:
-                y = associations.find(k)
+            kk = ''
+            kk += key
             for l in msg:
-                xy = associations[associations.find(l)+ associations.find(k)]
+                x = associations.find(l)
+                if len(msg) > len(kk):
+                    while True:
+                        kk += key
+                        if len(kk) > len(msg):
+                            break
+                    for k in kk:
+                        y = associations.find(k)
+                else:
+                    for k in key:
+                        y = associations.find(k)
+                        
+                xy = associations[x+associations.find(k)]
+                #if xy > len(associations):
                 p += str(xy)
             print(p)
             prompt()
         elif i == 'd':
             msg = input('Message: ')
             key = input('Key: ')
-            
+            p = ''
+            for l in msg:
+                x = associations.find(l)
+                for k in key:
+                    y = associations.find(k)
+                xy = associations[x-associations.find(k)]
+                p += str(xy)
+            print(p)
+            prompt()
         elif i == 'q':
             print('Goodbye!')
         else:
